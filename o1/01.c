1e9cab941ab5195fa80fbbca9028a3dffb99ab07
Vetle Roos Mangrud <vmangrud@gmail.com>
2022-02-02 15:48:36 +0100

    Lagde et lite skjelett 💀

--- o1/01.c
+++ o1/01.c
@@ -1,15 +1,63 @@
+#include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
 
+/* simple helper-function to empty stdin https://stackoverflow.com/a/53059527*/
+void empty_stdin (void) 
+{
+    int c = getchar();
+
+    while (c != '\n' && c != EOF)
+        c = getchar();
+}
+
+void schedule() {
+
+}
+
+void list() {
+
+}
+
+void cancel() {
+
+}
+
 int main() {
-   // printf() displays the string inside quotation
-   time_t t;   // not a primitive datatype
-   time(&t);
-   printf("Welcome to the alarm clock! It is currently %l %l Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)", t,  t);
+   time_t timer;
+   char buffer[26];
+   struct tm* tm_info;
+
+   timer = time(NULL);
+   tm_info = localtime(&timer);
+
+   strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
+   printf("Welcome to the alarm clock! It is currently %s Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n", buffer);
    
-   char choice;
-   scanf("%c", &choice);
-   printf("%c \n",choice);
+   char choice = getchar();
+   empty_stdin();
+   while (choice != 's' && choice != 'l' && choice != 'c' && choice != 'x')
+   {
+      printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n" );
+      choice = getchar();
+      empty_stdin();
+   }
+   
+   switch(choice)
+   {
+      case 's':
+         schedule();
+         break;
+      case 'l':
+         list();
+         break;
+      case 'c':
+         cancel();
+         break;
+      case 'x':
+         exit(0);
+         break;
+   }
    
    return 0;
 }
