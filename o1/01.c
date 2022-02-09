c40276760e5c542e0ffd1e1bf47c5f648db77529
Halvor Linder Henriksen <halvor_lh@hotmail.no>
2022-02-09 10:38:37 +0100

    Add timer scheduling

--- o1/01.c
+++ o1/01.c
@@ -1,6 +1,16 @@
+#define __USE_XOPEN ;
+#define _GNU_SOURCE ;
 #include<stdlib.h>
 #include<stdio.h>
 #include<time.h>
+#include<unistd.h>
+
+struct Alarm {
+   time_t end_time;
+   int pid;
+};
+
+struct Alarm alarms[100];
 
 /* simple helper-function to empty stdin https://stackoverflow.com/a/53059527*/
 void empty_stdin (void) 
@@ -12,7 +22,26 @@ void empty_stdin (void)
 }
 
 void schedule() {
-
+   struct Alarm new_alarm;
+   printf("Schedule alarm at which date and time? ");
+   char timestring[19];
+   struct tm timestruct;
+   scanf("%19[^\n]", timestring);
+   strptime(timestring, "%Y-%m-%d %H:%M:%S", &timestruct);
+   time_t unixtimestamp = mktime(&timestruct);
+   long secondsleft = unixtimestamp-time(NULL);
+   printf("Setting an alarm in %ld seconds \n", secondsleft);
+   new_alarm.end_time = unixtimestamp;
+   int pid = fork();
+   if(pid==0){
+      sleep(secondsleft);
+      printf("\a");
+      exit(0);
+   }
+   else{
+      //in parent
+      new_alarm.pid = pid;
+   }
 }
 
 void list() {
@@ -32,32 +61,34 @@ int main() {
    tm_info = localtime(&timer);
 
    strftime(buffer, 26, "%Y-%m-%d %H:%M:%S", tm_info);
-   printf("Welcome to the alarm clock! It is currently %s Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n", buffer);
-   
-   char choice = getchar();
-   empty_stdin();
-   while (choice != 's' && choice != 'l' && choice != 'c' && choice != 'x')
-   {
-      printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n" );
-      choice = getchar();
+   printf("Welcome to the alarm clock!");
+   while(1){
+      printf("It is currently %s Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n", buffer);
+      char choice = getchar();
       empty_stdin();
-   }
-   
-   switch(choice)
-   {
-      case 's':
-         schedule();
-         break;
-      case 'l':
-         list();
-         break;
-      case 'c':
-         cancel();
-         break;
-      case 'x':
-         exit(0);
-         break;
-   }
+      while (choice != 's' && choice != 'l' && choice != 'c' && choice != 'x')
+      {
+         printf("Please enter \"s\" (schedule), \"l\" (list), \"c\" (cancel), \"x\" (exit)\n" );
+         choice = getchar();
+         empty_stdin();
+      }
+      
+      switch(choice)
+      {
+         case 's':
+            schedule();
+            break;
+         case 'l':
+            list();
+            break;
+         case 'c':
+            cancel();
+            break;
+         case 'x':
+            exit(0);
+            break;
+      }
+   }   
    
    return 0;
 }
