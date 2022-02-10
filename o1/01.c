edfb0f2a18b8f837fc8efe32517903f617428ec4
Ivar Nakken <ivarnakken1441@gmail.com>
2022-02-10 14:27:54 +0100

    Fix handling of ended alarms and other minor fixes; e.g. color

--- o1/01.c
+++ o1/01.c
@@ -4,6 +4,7 @@
 #include <signal.h>
 #include <stdlib.h>
 #include <stdio.h>
+#include <sys/wait.h>
 #include <time.h>
 #include <unistd.h>
 
@@ -13,7 +14,7 @@ struct Alarm
    int pid;
 };
 
-/* Enum members are constant expressions, thus anonymous enum will work as constants */
+/* Enum members are constant expressions, thus anonymous enums will work as constants */
 enum
 {
    MAX_ALARMS = 5,
@@ -22,14 +23,21 @@ enum
 struct Alarm alarms[MAX_ALARMS];
 int amountOfAlarms = 0;
 
-void removeAlarmFromArray(int index) 
+void errorMessage(char *message)
 {
-   // Checking if input is in range
+   printf("\033[1;31m"); // Red
+   printf("%s", message);
+   printf("\033[0m"); // Black
+}
+
+void removeAlarmFromArray(int index)
+{
+   // Check if input is in range
    if (index < amountOfAlarms)
    {
       kill(alarms[index].pid, 0);
-      // Removing the element from the alarms array
-      for (int j = index; j < amountOfAlarms; j++)
+      // Remove the element from the alarms array
+      for (size_t j = index; j < amountOfAlarms; j++)
       {
          alarms[j] = alarms[j + 1];
       }
@@ -37,7 +45,19 @@ void removeAlarmFromArray(int index)
    }
 }
 
-/* Simple helper-function to empty stdin https://stackoverflow.com/a/53059527 */
+void handleEndedAlarm(int signal)
+{
+   int pid = wait(NULL);
+   for (size_t i = 0; i < amountOfAlarms; i++)
+   {
+      if (alarms[i].pid == pid)
+      {
+         removeAlarmFromArray(i);
+      }
+   }
+}
+
+/* Simple helper-function to empty stdin: https://stackoverflow.com/a/53059527 */
 void empty_stdin(void)
 {
    int c = getchar();
@@ -50,10 +70,10 @@ void schedule()
 {
    if (amountOfAlarms == MAX_ALARMS)
    {
-      printf("Too many alarms! Remove an alarm by typing \"c\" \n");
+      errorMessage("Too many alarms! Remove an alarm by typing \"c\" \n");
       return;
    }
-   // Timezone crap
+   // Adjust according to timezones
    time_t timer = time(NULL);
    struct tm *currentLocalTimeStruct;
    struct tm *currentUnixTimeStruct;
@@ -73,40 +93,34 @@ void schedule()
    // Load the user input into a time struct
    printf("Schedule alarm at which date and time? ");
    scanf("%19[^\n]", timestring);
+   empty_stdin();
    strptime(timestring, "%Y-%m-%d %H:%M:%S", &alarmTimeStruct);
 
-   // calculate the timer length in seconds
+   // Calculate the timer length in seconds
    alarmTimeStruct.tm_gmtoff = 0;
    alarmTimeStruct.tm_isdst = 0;
    time_t alarmUnixTime = mktime(&alarmTimeStruct) - totalOffset;
    long secondsleft = alarmUnixTime - time(NULL);
 
-   empty_stdin();
+   if (secondsleft < 0)
+   {
+      errorMessage("You cannot go back in time!\n");
+      return;
+   }
+
    printf("Setting an alarm in %ld seconds \n", secondsleft);
    new_alarm.end_time = alarmUnixTime;
 
    int pid = fork();
    if (pid == 0)
-   {  
+   {
       sleep(secondsleft);
       execlp("mpg123", "mpg123", "-q", "alarm.mp3", NULL);
-      // Remove alarm from array
-      for (size_t i = 0; i < amountOfAlarms; i++)
-      {
-         if (alarms[i].pid == getpid())
-         {
-            removeAlarmFromArray(i);
-         }
-         
-      }
-      
-      exit(0);
    }
    else
    {
-      // in parent
       // Stop zombies (We ignore exit codes, but oh well)
-      signal(SIGCHLD,SIG_IGN);
+      signal(SIGCHLD, handleEndedAlarm);
       new_alarm.pid = pid;
       alarms[amountOfAlarms] = new_alarm;
       amountOfAlarms++;
@@ -115,11 +129,12 @@ void schedule()
 
 void list()
 {
-   if (amountOfAlarms == 0)
+   if (!amountOfAlarms)
    {
-      printf("No alarms to show!\n");
+      errorMessage("No alarms to show!\n");
+      return;
    }
-   
+
    for (size_t i = 0; i < amountOfAlarms; i++)
    {
       char timestring[30];
@@ -130,12 +145,16 @@ void list()
 
 void cancel()
 {
+   if (!amountOfAlarms)
+   {
+      errorMessage("No alarms to cancel!\n");
+      return;
+   }
    int cancelledAlarm;
    printf("Cancel which alarm?\n> ");
    scanf("%d", &cancelledAlarm);
    empty_stdin();
    removeAlarmFromArray(cancelledAlarm - 1);
-   
 }
 
 int main()
@@ -143,7 +162,9 @@ int main()
    time_t timer;
    char buffer[26];
 
+   printf("\033[1;33m"); // Yellow
    printf("Welcome to the alarm clock!\n");
+   printf("\033[0m"); // Black
    while (1)
    {
       timer = time(NULL);
@@ -172,7 +193,7 @@ int main()
          break;
       case 'x':
          printf("Goodbye!\n");
-         exit(0);
+         _exit(0);
          break;
       }
    }
