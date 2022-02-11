91158893b8fd9f5060dce089b1138ba9a06fd8b3
Ivar Nakken <ivarnakken1441@gmail.com>
2022-02-11 13:14:37 +0100

    Minor fixes

--- o1/01.c
+++ o1/01.c
@@ -21,38 +21,38 @@ enum
 };
 
 struct Alarm alarms[MAX_ALARMS];
-int amountOfAlarms = 0;
+int active_alarms = 0;
 
-void errorMessage(char *message)
+void error_message(char *message)
 {
    printf("\033[1;31m"); // Red
    printf("%s", message);
    printf("\033[0m"); // Black
 }
 
-void removeAlarmFromArray(int index)
+void remove_alarm(int index)
 {
    // Check if input is in range
-   if (index < amountOfAlarms)
+   if (index < active_alarms)
    {
       kill(alarms[index].pid, 0);
       // Remove the element from the alarms array
-      for (size_t j = index; j < amountOfAlarms; j++)
+      for (size_t j = index; j < active_alarms; j++)
       {
          alarms[j] = alarms[j + 1];
       }
-      amountOfAlarms--;
+      active_alarms--;
    }
 }
 
-void handleEndedAlarm(int signal)
+void handle_ended_alarm()
 {
    int pid = wait(NULL);
-   for (size_t i = 0; i < amountOfAlarms; i++)
+   for (size_t i = 0; i < active_alarms; i++)
    {
       if (alarms[i].pid == pid)
       {
-         removeAlarmFromArray(i);
+         remove_alarm(i);
       }
    }
 }
@@ -68,93 +68,93 @@ void empty_stdin(void)
 
 void schedule()
 {
-   if (amountOfAlarms == MAX_ALARMS)
+   if (active_alarms == MAX_ALARMS)
    {
-      errorMessage("Too many alarms! Remove an alarm by typing \"c\" \n");
+      error_message("Too many alarms! Remove an alarm by typing \"c\" \n");
       return;
    }
    // Adjust according to timezones
    time_t timer = time(NULL);
-   struct tm *currentLocalTimeStruct;
-   struct tm *currentUnixTimeStruct;
-   currentLocalTimeStruct = localtime(&timer);
-   currentUnixTimeStruct = gmtime(&timer);
-   int daylightFlag = currentLocalTimeStruct->tm_isdst;
-   int timeZoneOffset = currentUnixTimeStruct->tm_gmtoff;
-   int totalOffset = (daylightFlag ? 3600 : 0) + timeZoneOffset;
-   int currentUnixTime = mktime(currentLocalTimeStruct);
-   int currentLocalTime = currentUnixTime + totalOffset;
+   struct tm *current_local_time_struct;
+   struct tm *current_unix_time_struct;
+   current_local_time_struct = localtime(&timer);
+   current_unix_time_struct = gmtime(&timer);
+   int daylight_flag = current_local_time_struct->tm_isdst;
+   int timezone_offset = current_unix_time_struct->tm_gmtoff;
+   int total_offset = (daylight_flag ? 3600 : 0) + timezone_offset;
+   int current_unix_time = mktime(current_local_time_struct);
+   int current_local_time = current_unix_time + total_offset;
 
    struct Alarm new_alarm;
    printf("Schedule alarm at which date and time? (yyyy-mm-dd HH:MM:SS)\n> ");
-   char timestring[19];
-   struct tm alarmTimeStruct;
+   char time_s[19];
+   struct tm alarm_time_struct;
 
    // Load the user input into a time struct
    printf("Schedule alarm at which date and time? ");
-   scanf("%19[^\n]", timestring);
+   scanf("%19[^\n]", time_s);
    empty_stdin();
-   strptime(timestring, "%Y-%m-%d %H:%M:%S", &alarmTimeStruct);
+   strptime(time_s, "%Y-%m-%d %H:%M:%S", &alarm_time_struct);
 
    // Calculate the timer length in seconds
-   alarmTimeStruct.tm_gmtoff = 0;
-   alarmTimeStruct.tm_isdst = 0;
-   time_t alarmUnixTime = mktime(&alarmTimeStruct) - totalOffset;
-   long secondsleft = alarmUnixTime - time(NULL);
+   alarm_time_struct.tm_gmtoff = 0;
+   alarm_time_struct.tm_isdst = 0;
+   time_t alarm_unix_time = mktime(&alarm_time_struct) - total_offset;
+   long seconds_left = alarm_unix_time - time(NULL);
 
-   if (secondsleft < 0)
+   if (seconds_left < 0)
    {
-      errorMessage("You cannot go back in time!\n");
+      error_message("You cannot go back in time!\n");
       return;
    }
 
-   printf("Setting an alarm in %ld seconds \n", secondsleft);
-   new_alarm.end_time = alarmUnixTime;
+   printf("Setting an alarm in %ld seconds \n", seconds_left);
+   new_alarm.end_time = alarm_unix_time;
 
    int pid = fork();
    if (pid == 0)
    {
-      sleep(secondsleft);
+      sleep(seconds_left);
       execlp("mpg123", "mpg123", "-q", "alarm.mp3", NULL);
    }
    else
    {
       // Stop zombies (We ignore exit codes, but oh well)
-      signal(SIGCHLD, handleEndedAlarm);
+      signal(SIGCHLD, handle_ended_alarm);
       new_alarm.pid = pid;
-      alarms[amountOfAlarms] = new_alarm;
-      amountOfAlarms++;
+      alarms[active_alarms] = new_alarm;
+      active_alarms++;
    }
 }
 
 void list()
 {
-   if (!amountOfAlarms)
+   if (!active_alarms)
    {
-      errorMessage("No alarms to show!\n");
+      error_message("No alarms to show!\n");
       return;
    }
 
-   for (size_t i = 0; i < amountOfAlarms; i++)
+   for (size_t i = 0; i < active_alarms; i++)
    {
-      char timestring[30];
-      strftime(timestring, 26, "%Y-%m-%d %H:%M:%S", localtime(&alarms[i].end_time));
-      printf("Alarm %ld at %s\n", i + 1, timestring);
+      char time_s[30];
+      strftime(time_s, 26, "%Y-%m-%d %H:%M:%S", localtime(&alarms[i].end_time));
+      printf("Alarm %ld at %s\n", i + 1, time_s);
    }
 }
 
 void cancel()
 {
-   if (!amountOfAlarms)
+   if (!active_alarms)
    {
-      errorMessage("No alarms to cancel!\n");
+      error_message("No alarms to cancel!\n");
       return;
    }
-   int cancelledAlarm;
+   int cancelled_alarm;
    printf("Cancel which alarm?\n> ");
-   scanf("%d", &cancelledAlarm);
+   scanf("%d", &cancelled_alarm);
    empty_stdin();
-   removeAlarmFromArray(cancelledAlarm - 1);
+   remove_alarm(cancelled_alarm - 1);
 }
 
 int main()
