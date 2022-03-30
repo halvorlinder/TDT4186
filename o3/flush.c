337e07a690ff54ace57e0ad736cfcbab40d79249
Ivar Nakken <ivarnakken1441@gmail.com>
2022-03-30 14:24:06 +0200

    Add change directory task 3.2

--- o3/flush.c
+++ o3/flush.c
@@ -9,16 +9,25 @@
 #define MAX_ARG_LENGTH 40
 #define MAX_COMMAND_LENGTH 10
 
+void change_dir(char *cwd, char *input)
+{
+    if (!input[0])
+    {
+        chdir(getenv("HOME"));
+    }
+    else
+    {
+        chdir(input);
+    }
+}
+
 int main(int argc, char const *argv[])
 {
     char dir[MAX_PATH];
     char args[MAX_ARGS][MAX_ARG_LENGTH];
     char raw_input[MAX_COMMAND_LENGTH + MAX_ARGS * MAX_ARG_LENGTH];
     char command[MAX_COMMAND_LENGTH];
-<<<<<<< HEAD
-=======
 
->>>>>>> Add docs and format
     while (1)
     {
         for (int i = 0; i < MAX_ARGS; i++)
@@ -30,15 +39,16 @@ int main(int argc, char const *argv[])
 
         // Print current working directory
         getcwd(dir, MAX_PATH);
-        printf("%s:", dir);
+        printf("%s: ", dir);
 
         // Get input from user, and extract the first "word", also called a token
         fgets(raw_input, MAX_COMMAND_LENGTH + MAX_ARGS + MAX_ARG_LENGTH, stdin);
+        raw_input[strcspn(raw_input, "\n")] = 0;
+
         char *token = strtok(raw_input, " ");
 
-        // Set command equal to the token's executable binary in /bin/
-        strcpy(command, "/bin/");
-        strcat(command, token);
+        // Set command equal to the token, should be equal to the executable binary in /bin/
+        strcpy(command, token);
 
         token = strtok(NULL, " "); // strtok() remembers raw_input, thus by applying NULL we get the next token
         int i = 0;
@@ -49,17 +59,24 @@ int main(int argc, char const *argv[])
             i++;
         }
 
-        int status;
-        int pid = fork();
-        if (pid == 0)
+        if (!strcmp(command, "cd"))
         {
-            execl(command, command, args, NULL);
-            _exit(0);
+            change_dir(dir, args[0]);
         }
         else
         {
-            waitpid(pid, &status, 0);
-            printf("Exit status: %d\n", status);
+            int status;
+            int pid = fork();
+            if (pid == 0)
+            {
+                execl(command, command, args, NULL);
+                _exit(0);
+            }
+            else
+            {
+                waitpid(pid, &status, 0);
+                printf("Exit status: %d\n", status);
+            }
         }
     }
 
