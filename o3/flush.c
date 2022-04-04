4f16d0fd9be2876a84e3d87db33f8ec01e127048
Halvor Linder Henriksen <halvor_lh@hotmail.no>
2022-04-04 19:06:30 +0200

    Implement io
    Implemented a wonky io redirectionwith a lot of bugs

--- o3/flush.c
+++ o3/flush.c
@@ -3,13 +3,17 @@
 #include <unistd.h>
 #include <string.h>
 #include <sys/wait.h>
+#include <fcntl.h>
+#include <ctype.h>
+
 
 #define MAX_PATH 200
 #define MAX_ARGS 20
 #define MAX_ARG_LENGTH 40
 #define MAX_COMMAND_LENGTH 10
+#define MAX_PIPELINE_LENGTH 10
 
-void change_dir(char *cwd, char *input)
+void change_dir(char *input)
 {
     if (!input)
     {
@@ -21,24 +25,97 @@ void change_dir(char *cwd, char *input)
     }
 }
 
-int main(int argc, char const *argv[])
+void strip(char* string){
+    puts("hello");
+    int start, end = -1;
+    for(int i = 0; i<strlen(string); i++){
+        if(start==-1 && !isspace(string[i])){
+            start = i;
+        }
+        if(!isspace(string[i])){
+            end = i;
+       }
+    }
+    string[end+1] = 0;
+    memcpy(string, &string[start], end-start+1);
+}
+
+void execute_cmd()
 {
-    char dir[MAX_PATH];
-    char args[MAX_ARGS][MAX_ARG_LENGTH];
     char raw_input[MAX_COMMAND_LENGTH + MAX_ARGS * MAX_ARG_LENGTH];
 
-    while (1)
+    fgets(raw_input, MAX_COMMAND_LENGTH + MAX_ARGS + MAX_ARG_LENGTH, stdin);
+    raw_input[strcspn(raw_input, "\n")] = 0;
+
+    char *token_pipe = strtok(raw_input, "|");
+    while (token_pipe != NULL)
     {
-        // Print current working directory
-        getcwd(dir, MAX_PATH);
-        printf("%s: ", dir);
+        int j = 0;
+        int input_index, output_index, end_index;
+        input_index = output_index = end_index = -1;
+        char input_file_name[MAX_ARG_LENGTH];
+        char output_file_name[MAX_ARG_LENGTH];
+        // if cd or jobs do something
+        //  if (!strcmp(args_cpy[0], "cd"))
+        //  {
+        //      change_dir(args_cpy[1]);
+        //  }
 
-        // Get input from user, and extract the first "word", also called a token
-        fgets(raw_input, MAX_COMMAND_LENGTH + MAX_ARGS + MAX_ARG_LENGTH, stdin);
-        raw_input[strcspn(raw_input, "\n")] = 0;
+        //Find the indices of the redirection operators
+        // puts(token_pipe);
+        while (j < MAX_ARG_LENGTH)
+        {
+            // printf("%c\n", token_pipe[j]);
+            if (token_pipe[j] == '>')
+            {
+                output_index = j;
+            }
+            else if (token_pipe[j] == '<')
+            {
+                input_index = j;
+            }
+            else if (token_pipe[j] == 0)
+            {
+                end_index = j;
+                break;
+            }
+            j++;
+            }
 
-        char *token = strtok(raw_input, " ");
-
+        // printf("%d\n", input_index);
+        // printf("%d\n", output_index);
+        // printf("%d\n", end_index);
+        
+        //Get the file names of the input/output files 
+        if (output_index > 0 && input_index > 0)
+        {
+            if (output_index > input_index)
+            {
+                memcpy(input_file_name, &token_pipe[input_index + 1], output_index-input_index-1);
+                memcpy(output_file_name, &token_pipe[output_index + 1], end_index-output_index-1);
+            }
+            else
+            {
+                memcpy(output_file_name, &token_pipe[output_index + 1], input_index-output_index-1);
+                memcpy(input_file_name, &token_pipe[input_index + 1], end_index-input_index-1);
+            }
+        }
+        else if (output_index > 0)
+        {
+            memcpy(output_file_name, &token_pipe[output_index + 1], end_index-output_index-1);
+        }
+        else if (input_index > 0)
+        {
+            memcpy(input_file_name, &token_pipe[input_index + 1], end_index-input_index-1);
+        }
+        
+        char *command_with_args = strtok(token_pipe, "<>");
+        puts(command_with_args);
+        puts(output_file_name);
+        puts(input_file_name);
+        //Parse the command 
+        char args[MAX_ARGS][MAX_ARG_LENGTH];
+        char *token = strtok(command_with_args, " ");
         int i = 0;
         while (token != NULL)
         {
@@ -46,34 +123,60 @@ int main(int argc, char const *argv[])
             token = strtok(NULL, " ");
             i++;
         }
-        
 
-        char* args_cpy[i];
-        for(int j = 0; j<i; j++)
+        char *args_cpy[i];
+        for (int j = 0; j < i; j++)
         {
             args_cpy[j] = malloc(MAX_ARG_LENGTH);
             strcpy(args_cpy[j], args[j]);
         }
+
         args_cpy[i] = NULL;
-        if (!strcmp(args_cpy[0], "cd"))
+
+        int status;
+        int pid = fork();
+        if (pid == 0)
         {
-            change_dir(dir, args_cpy[1]);
+            if (output_index > 0)
+            {
+                // strip(output_file_name);
+                int out = open(output_file_name, O_WRONLY);
+                dup2(out, STDOUT_FILENO);
+            }
+            if (input_index > 0)
+            {
+                // strip(input_file_name);
+                int in = open(input_file_name, O_RDONLY);
+                dup2(in, STDIN_FILENO);
+            }
+            status = execvp(args_cpy[0], args_cpy);
+            _exit(status);
         }
         else
         {
-            int status;
-            int pid = fork();
-            if (pid == 0)
-            {
-                status = execv(args_cpy[0], args_cpy);
-                _exit(status);
-            }
-            else
-            {
-                waitpid(pid, &status, 0);
-                printf("Exit status: %d\n", status);
-            }
+            waitpid(pid, &status, 0);
+            printf("Exit status: %d\n", status);
         }
+
+        for (int j = 0; j < i; j++)
+        {
+            free(args_cpy[j]);
+        }
+        i++;
+        token_pipe = strtok(NULL, "|");
+    }
+}
+
+int main(int argc, char const *argv[])
+{
+
+    char dir[MAX_PATH];
+    while (1)
+    {
+        // Print current working directory
+        getcwd(dir, MAX_PATH);
+        printf("%s: ", dir);
+        execute_cmd();
     }
 
     return 0;
