563526c658288d7a878a831241b9b5ba01c532f1
Halvor Linder Henriksen <halvor_lh@hotmail.no>
2022-04-04 20:33:01 +0200

    Fix file name stripping
    Fixed and uncommneded the function that strips a filename of whitespaces

--- o3/flush.c
+++ o3/flush.c
@@ -26,24 +26,24 @@ void change_dir(char *input)
 }
 
 void strip(char* string){
-    puts("hello");
-    int start, end = -1;
+    int start = -1;
+    int end = -1;
     for(int i = 0; i<strlen(string); i++){
-        if(start==-1 && !isspace(string[i])){
+        if(start==-1 && isspace(string[i])==0){
             start = i;
         }
         if(!isspace(string[i])){
             end = i;
        }
     }
-    string[end+1] = 0;
     memcpy(string, &string[start], end-start+1);
+    string[end-start+1] = 0;
 }
 
 void execute_cmd()
 {
     char raw_input[MAX_COMMAND_LENGTH + MAX_ARGS * MAX_ARG_LENGTH];
-
+    
     fgets(raw_input, MAX_COMMAND_LENGTH + MAX_ARGS + MAX_ARG_LENGTH, stdin);
     raw_input[strcspn(raw_input, "\n")] = 0;
 
@@ -55,6 +55,8 @@ void execute_cmd()
         input_index = output_index = end_index = -1;
         char input_file_name[MAX_ARG_LENGTH];
         char output_file_name[MAX_ARG_LENGTH];
+        memset(input_file_name, 0, MAX_ARG_LENGTH);
+        memset(output_file_name, 0, MAX_ARG_LENGTH);
         // if cd or jobs do something
         //  if (!strcmp(args_cpy[0], "cd"))
         //  {
@@ -82,9 +84,6 @@ void execute_cmd()
             j++;
             }
 
-        // printf("%d\n", input_index);
-        // printf("%d\n", output_index);
-        // printf("%d\n", end_index);
         
         //Get the file names of the input/output files 
         if (output_index > 0 && input_index > 0)
@@ -110,9 +109,9 @@ void execute_cmd()
         }
         
         char *command_with_args = strtok(token_pipe, "<>");
-        puts(command_with_args);
-        puts(output_file_name);
-        puts(input_file_name);
+        // printf("command: %s\n",command_with_args);
+        // printf("out: %s\n",output_file_name);
+        // printf("in: %s\n",input_file_name);
         //Parse the command 
         char args[MAX_ARGS][MAX_ARG_LENGTH];
         char *token = strtok(command_with_args, " ");
@@ -139,13 +138,13 @@ void execute_cmd()
         {
             if (output_index > 0)
             {
-                // strip(output_file_name);
+                strip(output_file_name);
                 int out = open(output_file_name, O_WRONLY);
                 dup2(out, STDOUT_FILENO);
             }
             if (input_index > 0)
             {
-                // strip(input_file_name);
+                strip(input_file_name);
                 int in = open(input_file_name, O_RDONLY);
                 dup2(in, STDIN_FILENO);
             }
