033ca935836058c3b9514846b5b37b044e73ae2d
Halvor Linder Henriksen <halvor_lh@hotmail.no>
2022-04-18 21:53:07 +0200

    Co-authored-by: Ivar Nakken <ivarnakken@users.noreply.github.com> Co-authored-by: Vetle Roos Mangrud <vetlemangrud@users.noreply.github.com>

--- o3/flush.c
+++ o3/flush.c
@@ -31,11 +31,11 @@ void add_background_process(int pid, char *command_line)
     active_background_processes++;
 }
 
-void remove_background_process(int pid)
+int remove_background_process(int pid)
 {
-    // Removes processes matchin pid from the array, but does not terminate anything.
+    // Remove processes matchin pid from the array, but not terminate anything.
     int found = 0;
-    for (size_t i = 0; i < active_background_processes; i++)
+    for (int i = 0; i < active_background_processes; i++)
     {
         if (background_processes[i].pid == pid)
         {
@@ -47,6 +47,7 @@ void remove_background_process(int pid)
         }
     }
     active_background_processes -= found;
+    return found;
 }
 
 void check_for_zombies()
@@ -57,8 +58,10 @@ void check_for_zombies()
     // Resolve each zombie
     while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
     {
-        remove_background_process(pid);
-        printf("Process %d ended. Exit status: %d\n", pid, status);
+        if (remove_background_process(pid))
+        {
+            printf("Process %d ended. Exit status: %d\n", pid, status);
+        }
     }
 }
 
@@ -105,7 +108,7 @@ void strip(char *string)
 void execute_cmd()
 {
     char raw_input[MAX_COMMAND_LENGTH + MAX_ARGS * MAX_ARG_LENGTH];
-    // stores the different parrts of the pipeline
+    // Stores the different parrts of the pipeline
     char *pipes[MAX_PIPELINE_LENGTH];
     // Allocate mem to strings
     for (int i = 0; i < MAX_ARG_LENGTH; i++)
@@ -121,25 +124,33 @@ void execute_cmd()
     int pipefds[MAX_PIPELINE_LENGTH - 1][2];
 
     // Take input and strip
-    fgets(raw_input, MAX_COMMAND_LENGTH + MAX_ARGS + MAX_ARG_LENGTH, stdin);
+    if (fgets(raw_input, MAX_COMMAND_LENGTH + MAX_ARGS + MAX_ARG_LENGTH, stdin) == NULL)
+    {
+        // Exit on Ctrl-D
+        puts("\n");
+        exit(0);
+    }
     raw_input[strcspn(raw_input, "\n")] = 0;
     strip(raw_input);
+    // printf("raw: %s\n", raw_input);
 
-    // check for background job and remove ampersand
+    // Check for background job and remove ampersand
     int is_background_process = raw_input[strlen(raw_input) - 1] == '&';
+    // printf("is_back: %d\n", is_background_process);
 
     if (is_background_process)
     {
         raw_input[strlen(raw_input) - 1] = '\0';
     }
+    // printf("new raw: %s\n", raw_input);
 
-    // clear the memory of the pipeline strings
+    // Clear the memory of the pipeline strings
     for (int i = 0; i < MAX_PIPELINE_LENGTH; i++)
     {
         memset(pipes[i], 0, MAX_COMMAND_LENGTH + MAX_ARGS * MAX_ARG_LENGTH);
     }
 
-    // parse pipelineparts into array
+    // Parse pipelineparts into array
     char *token_pipe = strtok(raw_input, "|");
     int i = 0;
     while (token_pipe != NULL)
@@ -151,16 +162,17 @@ void execute_cmd()
         i++;
         token_pipe = strtok(NULL, "|");
     }
-    // iterate over all pipelineparts and excecute
-    for (int p = 0; p < i; p++)
+    int pipe_length = i;
+    // printf("pipelength: %d\n", pipe_length);
+    // Iterate over all pipelineparts and excecute
+    for (int p = 0; p < pipe_length; p++)
     {
-        printf("pipepart: %s \n",pipes[p]);
+        // printf("pipepart %d: %s \n", p, pipes[p]);
         // if (i > 1)
         // {
         //     if (p < i - 1)
         //         pipe(pipefds[i]);
         // }
-        int pipe_length = i;
         int j = 0;
         int input_index, output_index, end_index;
         input_index = output_index = end_index = -1;
@@ -187,6 +199,8 @@ void execute_cmd()
             }
             j++;
         }
+        // printf("  output_index: %d\n", output_index);
+        // printf("  input_index: %d\n", input_index);
         // Get the file names of the input/output files
         if (output_index > 0 && input_index > 0)
         {
@@ -209,23 +223,24 @@ void execute_cmd()
         {
             memcpy(input_file_name, &pipes[p][input_index + 1], end_index - input_index - 1);
         }
-        char *command_with_args = strtok(pipes[p], "<>");
-        // printf("command: %s\n", command_with_args);
-        // printf("out: %s\n", output_file_name);
-        // printf("in: %s\n", input_file_name);
 
-        // Parse the command into args array
+        char *command_with_args = strtok(pipes[p], "<>");
+        // printf("  output_filename: %s\n", output_file_name);
+        // printf("  input_filename: %s\n", input_file_name);
+        // printf("  command: %s\n", command_with_args);
+
+        // Parse the command into args array by spliting on (space || tab)
         char args[MAX_ARGS][MAX_ARG_LENGTH];
-        char *token = strtok(command_with_args, " ");
+        char *token = strtok(command_with_args, " \t");
         int i = 0;
         while (token != NULL)
         {
             strcpy(args[i], token);
-            token = strtok(NULL, " ");
+            token = strtok(NULL, " \t");
             i++;
         }
 
-        // format the command to be accepted by execvp
+        // Format the command to be accepted by execvp
         char *args_cpy[i];
         for (int j = 0; j < i; j++)
         {
@@ -235,7 +250,12 @@ void execute_cmd()
 
         args_cpy[i] = NULL;
 
-        // if cd or jobs do the appropriate commands and break from the pipeline loop
+        for (int k = 0; k < i; k++)
+        {
+            // printf("    arg %d: %s\n", k, args_cpy[k]);
+        }
+
+        // If cd or jobs, execute the appropriate commands and break from the pipeline loop
         if (strncmp(args_cpy[0], "cd", 2) == 0)
         {
             change_dir(args_cpy[1]);
@@ -244,31 +264,31 @@ void execute_cmd()
         else if (strncmp(args_cpy[0], "jobs", 4) == 0)
         {
             // Do jobs command
-            puts("Printing jobs: \n");
+            display_jobs();
             break;
         }
 
-        // fork a new process to execute the command
+        // Fork a new process to execute the command
         int status;
         int pid = fork();
         if (pid == 0)
         {
-            // redirect output
+            // Redirect output
             if (output_index > 0)
             {
                 strip(output_file_name);
                 int out = open(output_file_name, O_WRONLY | O_CREAT, S_IRWXU);
                 dup2(out, STDOUT_FILENO);
             }
-            // redirect input
+            // Redirect input
             if (input_index > 0)
             {
                 strip(input_file_name);
                 int in = open(input_file_name, O_RDONLY);
                 dup2(in, STDIN_FILENO);
             }
-            
-            // handle pipeline redirection if there are pipes
+
+            // Handle pipeline redirection if there are pipes
             if (pipe_length > 1)
             {
                 // printf("fds: %d, %d, %d, %d\n", pipefds[p - 1][0], pipefds[p - 1][1], pipefds[p][0], pipefds[p][1]);
@@ -293,16 +313,16 @@ void execute_cmd()
                 }
             }
 
-            //execute the command and exit with the exit code
+            // Execute the command and exit with the exit code
             status = execvp(args_cpy[0], args_cpy);
-            _exit(status);
+            exit(status);
         }
-        //if in the parent process and on the final pipeline job, either wait or do background process management
+        // If in the parent process and on the final pipeline job, either wait or do background process management
         else if (p == pipe_length - 1)
         {
             if (is_background_process)
             {
-                add_background_process(pid, token_pipe);
+                add_background_process(pid, raw_input);
             }
             else
             {
@@ -310,13 +330,13 @@ void execute_cmd()
                 printf("Exit status: %d\n", status);
             }
         }
-        //free memory 
+        // Free memory
         for (int j = 0; j < i; j++)
         {
             free(args_cpy[j]);
         }
     }
-    //free memory
+    // Free memory
     for (int i = 0; i < MAX_PIPELINE_LENGTH; i++)
     {
         free(pipes[i]);
