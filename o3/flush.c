549bd05ba633e15ea4bde5b7740b91c66234984f
Halvor Linder Henriksen <halvor_lh@hotmail.no>
2022-04-18 18:59:56 +0200

    Fix cd and parse jobs command

--- o3/flush.c
+++ o3/flush.c
@@ -6,7 +6,6 @@
 #include <fcntl.h>
 #include <ctype.h>
 
-
 #define MAX_PATH 200
 #define MAX_ARGS 20
 #define MAX_ARG_LENGTH 40
@@ -14,7 +13,8 @@
 #define MAX_PIPELINE_LENGTH 10
 #define MAX_BACKGROUND_PROCESSES 20
 
-struct Background_Process {
+struct Background_Process
+{
     int pid;
     char command_line[MAX_PATH];
 };
@@ -22,17 +22,18 @@ struct Background_Process {
 struct Background_Process background_processes[MAX_BACKGROUND_PROCESSES];
 int active_background_processes = 0;
 
-void add_background_process (int pid, char *command_line)
+void add_background_process(int pid, char *command_line)
 {
     struct Background_Process new_background_process;
     new_background_process.pid = pid;
-    strcpy(new_background_process.command_line, command_line);background_processes[active_background_processes] = new_background_process;
+    strcpy(new_background_process.command_line, command_line);
+    background_processes[active_background_processes] = new_background_process;
     active_background_processes++;
 }
 
-void remove_background_process (int pid)
+void remove_background_process(int pid)
 {
-    //Removes processes matchin pid from the array, but does not terminate anything.
+    // Removes processes matchin pid from the array, but does not terminate anything.
     int found = 0;
     for (size_t i = 0; i < active_background_processes; i++)
     {
@@ -42,12 +43,10 @@ void remove_background_process (int pid)
         }
         else if (found)
         {
-            background_processes[i-found] = background_processes[i];
+            background_processes[i - found] = background_processes[i];
         }
     }
-    active_background_processes-= found;
-    
-    
+    active_background_processes -= found;
 }
 
 void check_for_zombies()
@@ -55,7 +54,7 @@ void check_for_zombies()
     int pid;
     int status;
 
-    //Resolve each zombie
+    // Resolve each zombie
     while ((pid = waitpid(-1, &status, WNOHANG)) > 0)
     {
         remove_background_process(pid);
@@ -84,31 +83,35 @@ void change_dir(char *input)
     }
 }
 
-void strip(char* string){
+void strip(char *string)
+{
     int start = -1;
     int end = -1;
-    for(int i = 0; i<strlen(string); i++){
-        if(start==-1 && isspace(string[i])==0){
+    for (int i = 0; i < strlen(string); i++)
+    {
+        if (start == -1 && isspace(string[i]) == 0)
+        {
             start = i;
         }
-        if(!isspace(string[i])){
+        if (!isspace(string[i]))
+        {
             end = i;
-       }
+        }
     }
-    memcpy(string, &string[start], end-start+1);
-    string[end-start+1] = 0;
+    memcpy(string, &string[start], end - start + 1);
+    string[end - start + 1] = 0;
 }
 
 void execute_cmd()
 {
     char raw_input[MAX_COMMAND_LENGTH + MAX_ARGS * MAX_ARG_LENGTH];
-    
+
     fgets(raw_input, MAX_COMMAND_LENGTH + MAX_ARGS + MAX_ARG_LENGTH, stdin);
     raw_input[strcspn(raw_input, "\n")] = 0;
 
     char *token_pipe = strtok(raw_input, "|");
     while (token_pipe != NULL)
-    {   
+    {
         int is_background_process = token_pipe[strlen(token_pipe) - 1] == '&';
 
         if (is_background_process)
@@ -123,14 +126,9 @@ void execute_cmd()
         char output_file_name[MAX_ARG_LENGTH];
         memset(input_file_name, 0, MAX_ARG_LENGTH);
         memset(output_file_name, 0, MAX_ARG_LENGTH);
-        // if cd or jobs do something
-        //  if (!strcmp(args_cpy[0], "cd"))
-        //  {
-        //      change_dir(args_cpy[1]);
-        //  }
 
-        //Find the indices of the redirection operators
-        // puts(token_pipe);
+        // Find the indices of the redirection operators
+        //  puts(token_pipe);
         while (j < MAX_ARG_LENGTH)
         {
             // printf("%c\n", token_pipe[j]);
@@ -150,35 +148,34 @@ void execute_cmd()
             j++;
         }
 
-        
-        //Get the file names of the input/output files 
+        // Get the file names of the input/output files
         if (output_index > 0 && input_index > 0)
         {
             if (output_index > input_index)
             {
-                memcpy(input_file_name, &token_pipe[input_index + 1], output_index-input_index-1);
-                memcpy(output_file_name, &token_pipe[output_index + 1], end_index-output_index-1);
+                memcpy(input_file_name, &token_pipe[input_index + 1], output_index - input_index - 1);
+                memcpy(output_file_name, &token_pipe[output_index + 1], end_index - output_index - 1);
             }
             else
             {
-                memcpy(output_file_name, &token_pipe[output_index + 1], input_index-output_index-1);
-                memcpy(input_file_name, &token_pipe[input_index + 1], end_index-input_index-1);
+                memcpy(output_file_name, &token_pipe[output_index + 1], input_index - output_index - 1);
+                memcpy(input_file_name, &token_pipe[input_index + 1], end_index - input_index - 1);
             }
         }
         else if (output_index > 0)
         {
-            memcpy(output_file_name, &token_pipe[output_index + 1], end_index-output_index-1);
+            memcpy(output_file_name, &token_pipe[output_index + 1], end_index - output_index - 1);
         }
         else if (input_index > 0)
         {
-            memcpy(input_file_name, &token_pipe[input_index + 1], end_index-input_index-1);
+            memcpy(input_file_name, &token_pipe[input_index + 1], end_index - input_index - 1);
         }
-        
+
         char *command_with_args = strtok(token_pipe, "<>");
         // printf("command: %s\n",command_with_args);
         // printf("out: %s\n",output_file_name);
         // printf("in: %s\n",input_file_name);
-        //Parse the command 
+        // Parse the command
         char args[MAX_ARGS][MAX_ARG_LENGTH];
         char *token = strtok(command_with_args, " ");
         int i = 0;
@@ -197,6 +194,18 @@ void execute_cmd()
         }
 
         args_cpy[i] = NULL;
+        // if cd or jobs do something
+        if (strncmp(args_cpy[0], "cd", 2)==0)
+        {
+            change_dir(args_cpy[1]);
+            break;
+        }
+        else if (strncmp(args_cpy[1], "jobs", 4)==0)
+        {
+           // Do jobs command
+           puts("Printing jobs: \n");
+           break;
+        }
 
         int status;
         int pid = fork();
